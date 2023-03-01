ba94bdb89a4b90da233f6da8a29882b032e510a4
Anton <>
2023-03-01 10:17:35 +0100

    Add project files.

---
+++ For Övning 4.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv en textrad : ");
+            string text = Console.ReadLine();
+            for (int i = text.Length-1; i >=0; i--)
+            {
+                Console.WriteLine(text[i]);
+            }
+            
+        }
+    }
+}
